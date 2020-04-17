b8513f1771f8ea2938c6302555ae785a6511dffe
Vadim Patsev <basvasilich@yandex.ru>
2020-04-17 20:24:53 +0300

    week 4

--- week3_hash_tables/4_substring_equality/substring_equality.py
+++ week3_hash_tables/4_substring_equality/substring_equality.py
@@ -2,15 +2,36 @@
 
 import sys
 
-class Solver:
-	def __init__(self, s):
-		self.s = s
-	def ask(self, a, b, l):
-		return s[a:a+l] == s[b:b+l]
 
-s = sys.stdin.readline()
-q = int(sys.stdin.readline())
-solver = Solver(s)
-for i in range(q):
-	a, b, l = map(int, sys.stdin.readline().split())
-	print("Yes" if solver.ask(a, b, l) else "No")
+class Solver:
+    def __init__(self, s):
+        self.s = s
+        self.m1 = 10 ** 9 + 7
+        self.m2 = 10 ** 9 + 9
+        self.x1 = 2
+        self.x2 = 3
+        self.h1 = dict()
+        self.h2 = dict()
+        self.h1[0] = 0
+        self.h2[0] = 0
+
+        for i in range(1, len(self.s) + 1):
+            self.h1[i] = (self.x1 * self.h1[i - 1] + ord(s[i - 1])) % self.m1
+        for j in range(1, len(self.s)):
+            self.h2[j] = (self.x2 * self.h2[j - 1] + ord(s[j - 1])) % self.m2
+
+    def ask(self, a, b, l):
+        z = pow(self.x1, l, self.m1)
+        x = pow(self.x2, l, self.m2)
+        return (self.h1[a + l] - z * self.h1[a]) % self.m1 == (self.h1[b + l] - z * self.h1[b]) % self.m1 \
+               and (self.h2[a + l] - x * self.h2[a]) % self.m2 == (self.h2[b + l] - x * self.h2[b]) % self.m2
+
+
+if __name__ == '__main__':
+    s = sys.stdin.readline()
+    q = int(sys.stdin.readline())
+    solver = Solver(s)
+
+    for i in range(q):
+        a, b, l = map(int, sys.stdin.readline().split())
+        print("Yes" if solver.ask(a, b, l) else "No")
--- week4_binary_search_trees/2_is_bst/is_bst.py
+++ week4_binary_search_trees/2_is_bst/is_bst.py
@@ -2,22 +2,40 @@
 
 import sys, threading
 
-sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+sys.setrecursionlimit(10 ** 9)  # max depth of recursion
+threading.stack_size(2 ** 28)  # new thread will get stack of such size
+
 
 def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
-  return True
+    global prev
+    if len(tree) == 0:
+        return True
+
+    def helper(index):
+        global prev
+        if index == -1:
+            return True
+
+        if not helper(tree[index][1]):
+            return False
+
+        if prev is not None and prev > tree[index][0]:
+            return False
+        prev = tree[index][0]
+        return helper(tree[index][2])
+    prev = None
+    return helper(0)
 
 
 def main():
-  nodes = int(sys.stdin.readline().strip())
-  tree = []
-  for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
-    print("CORRECT")
-  else:
-    print("INCORRECT")
+    nodes = int(sys.stdin.readline().strip())
+    tree = []
+    for i in range(nodes):
+        tree.append(list(map(int, sys.stdin.readline().strip().split())))
+    if IsBinarySearchTree(tree):
+        print("CORRECT")
+    else:
+        print("INCORRECT")
+
 
 threading.Thread(target=main).start()
